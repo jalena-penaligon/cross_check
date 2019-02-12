29697838b41d59d72bb02ac1d916c49a993e74cd
wipegup <wipegup@gmail.com>
2019-02-12 16:16:07 -0700

    Add comments to guide for merge_data method

--- lib/stat_parser.rb
+++ lib/stat_parser.rb
@@ -4,5 +4,22 @@ class StatParser
     @merged_data = merge_data(array_raw_data, array_merge_keys)
   end
 
-  
+  def merge_data(array_raw_data, array_merge_keys)
+    ### array_raw_data is three arrays of hashes
+    ### 1st hash is of game_team data
+    ### 2nd hash is of  game data
+    ### 3rd hash is team_info data
+
+    ### array_merge_keys is two keys -- :game_id, :team_id
+
+    ### game_team and game are merged with :game_id
+    ### game/game_team and team_info are merged with :team_id
+
+    ### Desired keys are:
+    # {game_id, team_id, hoa, won, settled_in, head_coach,
+    #  goals, shots, hits, pim, powerplaygoals, powerplayopportunities,
+    #  faceoffwinpercentage, giveaways, takeaways, teamname, season,
+    #  type, date_time, venue_time_zone_tz}
+
+  end
 end
