cb4b17414203e2b9afbb3277b304781c9f68c636
wipegup <wipegup@gmail.com>
2019-02-13 12:12:16 -0700

    testing open_csv -- passing

--- test/stat_tracker_test.rb
+++ test/stat_tracker_test.rb
@@ -13,23 +13,25 @@ class StatTrackerTest < MiniTest::Test
     stat_tracker = StatTracker.new
 
     game_array = [
-      {game_id: '2012030221', season: '20122013', type: 'P', date_time: '2013-05-16',
+      {nil =>"0", game_id: '2012030221', season: '20122013', type: 'P', date_time: '2013-05-16',
         away_team_id: '3', home_team_id: '6', away_goals: '2', home_goals: '3',
         outcome: 'home win OT', home_rink_side_start: 'left', venue: 'TD Garden',
-        venue_time_zone_id: 'America/New_York', venue_time_zone_offset: '-4',
+        venue_link: "/api/v1/venues/null", venue_time_zone_id: 'America/New_York', venue_time_zone_offset: '-4',
         venue_time_zone_tz: 'EDT'},
-      {game_id: '2012030222', season: '20122013', type: 'P', date_time: '2013-05-19',
+      {nil =>"1", game_id: '2012030222', season: '20122013', type: 'P', date_time: '2013-05-19',
         away_team_id: '3', home_team_id: '6', away_goals: '2', home_goals: '5',
         outcome: 'home win REG', home_rink_side_start: 'left', venue: 'TD Garden',
-        venue_time_zone_id: 'America/New_York', venue_time_zone_offset: '-4',
+        venue_link: "/api/v1/venues/null", venue_time_zone_id: 'America/New_York', venue_time_zone_offset: '-4',
         venue_time_zone_tz: 'EDT'}
     ]
 
     game_path = './data/game_very_small.csv'
 
-    assert_equal game_array, stat_tracker.open_csv(game_path)
+    actual = stat_tracker.open_csv(game_path)
+
+    assert_equal game_array, actual
   end
-  
+
   def test_from_csv_works
     game_path = './data/game_very_small.csv'
     team_path = './data/team_info.csv'
