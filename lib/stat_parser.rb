7c0008ffbcd8b1d3811c88467c2c8b04cd1ae5c5
wipegup <wipegup@gmail.com>
2019-02-13 12:50:19 -0700

    Passing new merge and parse tests

--- lib/stat_parser.rb
+++ lib/stat_parser.rb
@@ -6,16 +6,24 @@ class StatParser
     @array_merge_keys = array_merge_keys
   end
 
+  def parse_data
+    merged_data = merge_data
+
+    converted_data = convert_data_types(merged_data)
+  end
+
   def merge_data
-    #game_team = @array_raw_data[0]
-    #game_data = @array_raw_data[1]
-    #team_info = @array_raw_data[2]
+    #[game_team, game, team] = @array_raw_data
 
     first_merge = merge_hash_arrays(@array_raw_data[0], @array_raw_data[1], @array_merge_keys[0])
 
-
     second_merge = merge_hash_arrays(first_merge, @array_raw_data[2], @array_merge_keys[1])
 
+    return second_merge
+  end
+
+
+  def convert_data_types(array_of_hashes)
     to_delete = [nil, :venue_time_zone_id, :venue_time_zone_offset, :home_rink_side_start, :franchiseid,
      :venue_link, :venue, :abbreviation, :link, :shortname, :away_team_id, :home_team_id, :outcome]
 
@@ -24,20 +32,17 @@ class StatParser
       :powerplayopportunities, :powerplaygoals, :giveaways, :takeaways]
     to_float = [:faceoffwinpercentage]
     to_boolean = [:won]
-    converted_data = convert_data_types(second_merge, to_delete, to_int,
-                                        to_float, to_boolean)
-    return second_merge
-  end
 
-  def convert_data_types(array_of_hashes, to_delete, to_int, to_float, to_boolean)
-     converted_values = array_of_hashes.map do |hash|
+    converted_values = array_of_hashes.map do |hash|
 
         to_delete.each do |key|
           hash.delete(key)
         end
 
         to_int.each do |key|
-          hash[key] = hash[key].to_i
+          if hash.keys.include?(key)
+            hash[key] = hash[key].to_i
+          end
         end
 
         to_boolean.each do |key|
@@ -49,7 +54,9 @@ class StatParser
         end
 
         to_float.each do |key|
-          hash[key] = hash[key].to_f
+          if hash.keys.include?(key)
+            hash[key] = hash[key].to_f
+          end
         end
 
       hash
