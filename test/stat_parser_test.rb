aa72dc5a951570351c408f932870598aa0d2cbef
wipegup <wipegup@gmail.com>
2019-02-13 14:37:12 -0700

    Add test for converting values in hash to integers

--- test/stat_parser_test.rb
+++ test/stat_parser_test.rb
@@ -107,6 +107,13 @@ class StatParserTest < MiniTest::Test
       assert_equal expected, actual
   end
 
+  def test_it_can_convert_values_to_int
+    simple_hash = {game_id: "0", season:"3", shots:"5", name:"Blackhawks"}
+
+    actual = @stat_parser.convert_to_int(simple_hash)
+    expected = {game_id: 0, shots: 5, season: 3, name: "Blackhawks"}}
+    assert_equal expected, actual
+  end
   def test_it_can_delete_keys
       simple_hash = {nil => 0, link:3, shots:5}
 
