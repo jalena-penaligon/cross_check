46fcf44c0d128619bde28b3ef7ca4319cee9ee06
wipegup <wipegup@gmail.com>
2019-02-13 15:03:37 -0700

    Conversions moved to module

--- lib/stat_parser.rb
+++ lib/stat_parser.rb
@@ -1,4 +1,5 @@
 require 'pry'
+require './lib/conversions'
 class StatParser
   include Conversions
   def initialize(array_raw_data, array_merge_keys)
@@ -35,59 +36,6 @@ class StatParser
     end
 
     return converted_values
-  end
-
-  def convert_to_boolean(hash)
-    to_boolean = [:won]
-
-    to_boolean.each do |key|
-      if hash[key] == "False"
-        hash[key] = false
-      else
-        hash[key] = true
-      end
-    end
-
-    return hash
-  end
-
-  def convert_to_float(hash)
-    to_float = [:faceoffwinpercentage]
-
-    to_float.each do |key|
-      if hash.keys.include?(key)
-        hash[key] = hash[key].to_f
-      end
-    end
-
-    return hash
-  end
-
-  def convert_to_int(hash)
-    to_int = [:game_id, :season, :away_goals,:home_goals, :team_id, :goals,
-              :shots, :hits, :pim, :powerplayopportunities, :powerplaygoals,
-              :giveaways, :takeaways]
-
-    to_int.each do |key|
-      if hash.keys.include?(key)
-        hash[key] = hash[key].to_i
-      end
-    end
-
-    return hash
-  end
-
-  def delete_keys(hash)
-    to_delete = [nil, :venue_time_zone_id, :venue_time_zone_offset,
-                :home_rink_side_start, :franchiseid, :venue_link, :venue,
-                :abbreviation, :link, :shortname, :away_team_id, :home_team_id,
-                :outcome]
-
-    to_delete.each do |key|
-      hash.delete(key)
-    end
-
-    return hash
   end
 
   def merge_hash_arrays(merge_to_array, merge_from_array, merge_key)
