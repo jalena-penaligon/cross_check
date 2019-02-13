870a6b31cc30a4c9debe7ce6e56267903e90c479
wipegup <wipegup@gmail.com>
2019-02-13 12:28:40 -0700

    Clean up 'from_csv' method

--- lib/stat_tracker.rb
+++ lib/stat_tracker.rb
@@ -14,15 +14,18 @@ class StatTracker
   def self.from_csv(locations)
     stat_tracker = StatTracker.new
 
-    game_teams = stat_tracker.open_csv(locations[:game_teams])
-    game = stat_tracker.open_csv(locations[:games])
-    team_info = stat_tracker.open_csv(locations[:teams])
+    raw_data = stat_tracker.open_all_csvs(locations)
 
-    raw_data = [game_teams, game, team_info]
     stat_parser = StatParser.new(raw_data, stat_tracker.merge_ids)
     stat_tracker.data = stat_parser.merge_data
 
     return stat_tracker
+  end
+
+  def open_all_csvs(locations)
+    return [open_csv(locations[:game_teams]),
+            open_csv(locations[:games]),
+            open_csv(locations[:teams])]
   end
 
   def open_csv(file_path)
