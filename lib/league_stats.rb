9276522719ab32605a2b370ce74d95385592c603
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-13 14:45:33 -0700

    Passed best offense and worse offense tests

--- lib/league_stats.rb
+++ lib/league_stats.rb
@@ -1,11 +1,27 @@
 module LeagueStats
 
   def count_of_teams
-    all_teams = []
-    @data.each do |game_team|
-      all_teams << game_team[:team_id]
-    end
-    all_teams.uniq.count
+    @data.map do |game_team|
+      game_team[:team_id]
+    end.uniq.count
   end
-  
+
+  def aggregate_team_goals
+    @data.inject({}) do |team_hash, game_team|
+      team_hash[game_team[:teamname]] = game_team[:goals]
+      team_hash
+    end
+  end
+
+  def best_offense
+    aggregate_team_goals.max_by do |team, scores|
+      scores
+    end.first
+  end
+
+  def worst_offense
+    aggregate_team_goals.min_by do |team, scores|
+      scores
+    end.first
+  end
 end
