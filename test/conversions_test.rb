da7cebaeafa68e37d733a2aabce1a78749ad52a7
wipegup <wipegup@gmail.com>
2019-02-14 14:43:45 -0700

    comment out unused tests

--- test/conversions_test.rb
+++ test/conversions_test.rb
@@ -6,7 +6,7 @@ class ConversionsTest < MiniTest::Test
     def setup
       @stat_parser = StatParser.new([],[])
     end
-    
+
     def test_it_can_convert_values_to_boolean
       simple_hash = {won: "False", season:"3", name:"Blackhawks"}
 
@@ -15,21 +15,21 @@ class ConversionsTest < MiniTest::Test
       assert_equal expected, actual
     end
 
-    def test_it_can_convert_values_to_int
-      simple_hash = {game_id: "0", season:"3", shots:"5", name:"Blackhawks"}
-
-      actual = @stat_parser.convert_to_int(simple_hash)
-      expected = {game_id: 0, shots: 5, season: 3, name: "Blackhawks"}
-      assert_equal expected, actual
-    end
-
-    def test_it_can_convert_values_to_floats
-      simple_hash = { faceoffwinpercentage:"52.5", shots:"5", name:"Blackhawks"}
-
-      actual = @stat_parser.convert_to_float(simple_hash)
-      expected = {faceoffwinpercentage:52.5, shots: "5", name: "Blackhawks"}
-      assert_equal expected, actual
-    end
+    # def test_it_can_convert_values_to_int
+    #   simple_hash = {game_id: "0", season:"3", shots:"5", name:"Blackhawks"}
+    #
+    #   actual = @stat_parser.convert_to_int(simple_hash)
+    #   expected = {game_id: 0, shots: 5, season: 3, name: "Blackhawks"}
+    #   assert_equal expected, actual
+    # end
+    #
+    # def test_it_can_convert_values_to_floats
+    #   simple_hash = { faceoffwinpercentage:"52.5", shots:"5", name:"Blackhawks"}
+    #
+    #   actual = @stat_parser.convert_to_float(simple_hash)
+    #   expected = {faceoffwinpercentage:52.5, shots: "5", name: "Blackhawks"}
+    #   assert_equal expected, actual
+    # end
 
     def test_it_can_delete_keys
         simple_hash = {nil => 0, link:3, shots:5}
