b4206ed2a7a9d1caa64a3bf594628f62839f2f48
wipegup <wipegup@gmail.com>
2019-02-14 15:52:18 -0700

    Change conversion tests for specific boolean name

--- test/conversions_test.rb
+++ test/conversions_test.rb
@@ -10,26 +10,10 @@ class ConversionsTest < MiniTest::Test
     def test_it_can_convert_values_to_boolean
       simple_hash = {won: "False", season:"3", name:"Blackhawks"}
 
-      actual = @stat_parser.convert_to_boolean(simple_hash)
+      actual = @stat_parser.convert_won_to_boolean(simple_hash)
       expected = {won:false, season: "3", name: "Blackhawks"}
       assert_equal expected, actual
     end
-
-    # def test_it_can_convert_values_to_int
-    #   simple_hash = {game_id: "0", season:"3", shots:"5", name:"Blackhawks"}
-    #
-    #   actual = @stat_parser.convert_to_int(simple_hash)
-    #   expected = {game_id: 0, shots: 5, season: 3, name: "Blackhawks"}
-    #   assert_equal expected, actual
-    # end
-    #
-    # def test_it_can_convert_values_to_floats
-    #   simple_hash = { faceoffwinpercentage:"52.5", shots:"5", name:"Blackhawks"}
-    #
-    #   actual = @stat_parser.convert_to_float(simple_hash)
-    #   expected = {faceoffwinpercentage:52.5, shots: "5", name: "Blackhawks"}
-    #   assert_equal expected, actual
-    # end
 
     def test_it_can_delete_keys
         simple_hash = {nil => 0, link:3, shots:5}
