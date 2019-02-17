ae619660781e6a78d1a5aea288c7dd3c54039b61
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-16 17:18:54 -0700

    Created team_info method to return hash of info

--- lib/team_stats.rb
+++ lib/team_stats.rb
@@ -1,3 +1,19 @@
 module TeamStats
 
+  def team_info(team_id)
+    team_info = {}
+    @data.each do |game_team|
+      info = {
+        team_id: game_team[:team_id],
+        franchise_id: game_team[:franchiseid],
+        short_name: game_team[:shortname],
+        team_name: game_team[:teamname],
+        abbreviation: game_team[:abbreviation],
+        link: game_team[:link]
+      }
+      team_info[game_team[:team_id]] = info
+    end
+    team_info[team_id]
+  end
+
 end
