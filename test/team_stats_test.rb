0e0ee7e2fc2f25021278351e36cb067491cbdfbe
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-16 17:25:16 -0700

    Wrote test to count team wins by season

--- test/team_stats_test.rb
+++ test/team_stats_test.rb
@@ -19,7 +19,7 @@ class TeamStatsTest < Minitest::Test
 
       @v_small_data = StatTracker.from_csv(v_small_data_paths)
       @small_data = StatTracker.from_csv(small_data_paths)
-      @data = StatTracker.from_csv(data_paths)
+      # @data = StatTracker.from_csv(data_paths)
     end
 
   def test_team_info_with_respective_team_attributes
@@ -43,135 +43,140 @@ class TeamStatsTest < Minitest::Test
                 }
     assert_equal expected, @small_data.team_info(16)
 
-    expected = {
-                team_id: 18,
-                franchise_id: 34,
-                short_name: "Nashville",
-                team_name: "Predators",
-                abbreviation: "NSH",
-                link: "/api/v1/teams/18"
-                }
-    assert_equal expected, @data.team_info(18)
+    # expected = {
+    #             team_id: 18,
+    #             franchise_id: 34,
+    #             short_name: "Nashville",
+    #             team_name: "Predators",
+    #             abbreviation: "NSH",
+    #             link: "/api/v1/teams/18"
+    #             }
+    # assert_equal expected, @data.team_info(18)
   end
 
-  def test_best_season
-    assert_equal 20122013, @v_small_data.best_season(6)
-    assert_equal 20122013, @small_data.best_season(5)
-    assert_equal 20142015, @data.best_season(8)
+  def test_count_team_wins_by_season
+    assert_equal 4, @v_small_data.wins_per_season(3)
+    assert_equal 2, @small_data.wins_per_season(6)
   end
 
-  def test_worst_season
-    assert_equal 20122013, @v_small_data.worst_season(3)
-    assert_equal 20122013, @small_data.worst_season(16)
-    assert_equal 20142015, @data.worst_season(5)
-  end
-
-  def test_average_win_percentage
-    assert_equal 1.0, @v_small_data.average_win_percentage(6)
-    assert_equal 0.75, @small_data.average_win_percentage(9)
-    assert_equal 0.447, @data.average_win_percentage(21)
-  end
-
-  def test_most_goals_scored
-    assert_equal 2, @v_small_data.most_goals_scored(3)
-    assert_equal 4, @small_data.most_goals_scored(17)
-    assert_equal 8, @data.most_goals_scored(2)
-  end
-
-  def test_fewest_goals_scored
-    assert_equal 3, @v_small_data.fewest_goals_scored(6)
-    assert_equal 1, @small_data.fewest_goals_scored(8)
-    assert_equal 0, @data.fewest_goals_scored(7)
-  end
-
-  def test_favorite_opponent
-    assert_equal "Bruins", @v_small_data.favorite_opponent(3)
-    assert_equal "Bruins", @small_data.favorite_opponent(5)
-    assert_equal "Coyotes", @data.favorite_opponent(12)
-  end
-
-  def test_rival
-    assert_equal "Rangers", @v_small_data.rival(6)
-    assert_equal "Bruins", @small_data.rival(3)
-    assert_equal "Blackhawks", @data.rival(5)
-  end
-
-  def test_biggest_team_blowout
-    assert_equal 3, @v_small_data.biggest_team_blowout(6)
-    assert_equal 3, @small_data.biggest_team_blowout(16)
-    assert_equal 8, @data.biggest_team_blowout(24)
-  end
-
-  def test_worst_loss
-    assert_equal -3, @v_small_data.worst_loss(3)
-    assert_equal -3, @small_data.worst_loss(17)
-    assert_equal -7, @data.worst_loss(27)
-  end
-
-  def test_head_to_head
-    expected = {"Rangers" => 1.0}
-    assert_equal expected, @v_small_data.head_to_head(6)
-
-    expected = {
-                "Penguins" => 1.0,
-                "Rangers" => 0.8,
-                }
-    assert_equal expected, @small_data.head_to_head(6)
-
-    expected = {
-      "Blackhawks" => 0.53,
-      "Blue Jackets" => 0.46,
-      "Blues" => 0.37,
-      "Bruins" => 0.7,
-      "Canadians" => 0.5,
-      "Canucks" => 0.42,
-      "Capitals" => 0.3,
-      "Coyotes" => 0.56,
-      "Devils" => 0.7,
-      "Ducks" => 0.39,
-      "Flames" => 0.42,
-      "Flyers" => 0.4,
-      "Golden Knights" => 0.33,
-      "Hurricanes" => 0.5,
-      "Islanders" => 0.5,
-      "Jets" => 0.5,
-      "Kings" => 0.28,
-      "Lightning" => 0.4,
-      "Maple Leafs" => 0.5,
-      "Oilers" => 0.4,
-      "Panthers" => 0.5,
-      "Penguins" => 0.5,
-      "Predators" => 0.38,
-      "Rangers" => 0.5,
-      "Red Wings" => 0.46,
-      "Sabres" => 0.7,
-      "Senators" => 0.4,
-      "Sharks" => 0.39,
-      "Stars" => 0.59,
-      "Wild" => 0.42,
-    }
-    assert_equal expected, @data.head_to_head(21)
-  end
-
-  def test_seasonal_summary
-    expected = {
-                20122013 => {
-                  preseason: {
-                              win_percentage:
-                              total_goals_scored:
-                              total_goals_against:
-                              average_goals_scored:
-                              average_goals_against:
-                              }
-                  regular_season:{
-                                  win_percentage:
-                                  total_goals_scored:
-                                  total_goals_against:
-                                  average_goals_scored:
-                                  average_goals_against:
-                                  }
-                                }
-                              }
-    assert_equal expected, @data.seasonal_summary(3)
-  end
+#   def test_best_season
+#     assert_equal 20122013, @v_small_data.best_season(6)
+#     assert_equal 20122013, @small_data.best_season(5)
+#     assert_equal 20142015, @data.best_season(8)
+#   end
+#
+#   def test_worst_season
+#     assert_equal 20122013, @v_small_data.worst_season(3)
+#     assert_equal 20122013, @small_data.worst_season(16)
+#     assert_equal 20142015, @data.worst_season(5)
+#   end
+#
+#   def test_average_win_percentage
+#     assert_equal 1.0, @v_small_data.average_win_percentage(6)
+#     assert_equal 0.75, @small_data.average_win_percentage(9)
+#     assert_equal 0.447, @data.average_win_percentage(21)
+#   end
+#
+#   def test_most_goals_scored
+#     assert_equal 2, @v_small_data.most_goals_scored(3)
+#     assert_equal 4, @small_data.most_goals_scored(17)
+#     assert_equal 8, @data.most_goals_scored(2)
+#   end
+#
+#   def test_fewest_goals_scored
+#     assert_equal 3, @v_small_data.fewest_goals_scored(6)
+#     assert_equal 1, @small_data.fewest_goals_scored(8)
+#     assert_equal 0, @data.fewest_goals_scored(7)
+#   end
+#
+#   def test_favorite_opponent
+#     assert_equal "Bruins", @v_small_data.favorite_opponent(3)
+#     assert_equal "Bruins", @small_data.favorite_opponent(5)
+#     assert_equal "Coyotes", @data.favorite_opponent(12)
+#   end
+#
+#   def test_rival
+#     assert_equal "Rangers", @v_small_data.rival(6)
+#     assert_equal "Bruins", @small_data.rival(3)
+#     assert_equal "Blackhawks", @data.rival(5)
+#   end
+#
+#   def test_biggest_team_blowout
+#     assert_equal 3, @v_small_data.biggest_team_blowout(6)
+#     assert_equal 3, @small_data.biggest_team_blowout(16)
+#     assert_equal 8, @data.biggest_team_blowout(24)
+#   end
+#
+#   def test_worst_loss
+#     assert_equal -3, @v_small_data.worst_loss(3)
+#     assert_equal -3, @small_data.worst_loss(17)
+#     assert_equal -7, @data.worst_loss(27)
+#   end
+#
+#   def test_head_to_head
+#     expected = {"Rangers" => 1.0}
+#     assert_equal expected, @v_small_data.head_to_head(6)
+#
+#     expected = {
+#                 "Penguins" => 1.0,
+#                 "Rangers" => 0.8,
+#                 }
+#     assert_equal expected, @small_data.head_to_head(6)
+#
+#     expected = {
+#       "Blackhawks" => 0.53,
+#       "Blue Jackets" => 0.46,
+#       "Blues" => 0.37,
+#       "Bruins" => 0.7,
+#       "Canadians" => 0.5,
+#       "Canucks" => 0.42,
+#       "Capitals" => 0.3,
+#       "Coyotes" => 0.56,
+#       "Devils" => 0.7,
+#       "Ducks" => 0.39,
+#       "Flames" => 0.42,
+#       "Flyers" => 0.4,
+#       "Golden Knights" => 0.33,
+#       "Hurricanes" => 0.5,
+#       "Islanders" => 0.5,
+#       "Jets" => 0.5,
+#       "Kings" => 0.28,
+#       "Lightning" => 0.4,
+#       "Maple Leafs" => 0.5,
+#       "Oilers" => 0.4,
+#       "Panthers" => 0.5,
+#       "Penguins" => 0.5,
+#       "Predators" => 0.38,
+#       "Rangers" => 0.5,
+#       "Red Wings" => 0.46,
+#       "Sabres" => 0.7,
+#       "Senators" => 0.4,
+#       "Sharks" => 0.39,
+#       "Stars" => 0.59,
+#       "Wild" => 0.42,
+#     }
+#     assert_equal expected, @data.head_to_head(21)
+#   end
+#
+#   def test_seasonal_summary
+#     expected = {
+#                 20122013 => {
+#                   preseason: {
+#                               win_percentage:
+#                               total_goals_scored:
+#                               total_goals_against:
+#                               average_goals_scored:
+#                               average_goals_against:
+#                               }
+#                   regular_season:{
+#                                   win_percentage:
+#                                   total_goals_scored:
+#                                   total_goals_against:
+#                                   average_goals_scored:
+#                                   average_goals_against:
+#                                   }
+#                                 }
+#                               }
+#     assert_equal expected, @data.seasonal_summary(3)
+#   end
 end
