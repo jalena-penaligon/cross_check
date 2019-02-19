812105a1f09ddfe07c55013b3d70f752edb125f2
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-19 15:35:00 -0700

    refactored last 5 methods using helpers module

--- lib/game_stats.rb
+++ lib/game_stats.rb
@@ -21,66 +21,38 @@ module GameStats
     total_score = (blowout_score[:goals] - blowout_score[:opponent_goals]).abs
   end
 
-  def percentage_home_wins
-    number_home_wins = @data.select do |hash|
-      (hash[:hoa] == "home") && (hash[:won] == true)
-    end
-    percent_home_wins = ((number_home_wins.count) / total_number_of_games).round(2)
+  def percent_home_wins(data = nil)
+    data = @data if data == nil
+    data = subset_data(:hoa, "home", data)
+    (winning_percentage(data)).round(2)
   end
 
-  def percentage_visitor_wins
-    number_visitor_wins = @data.select do |hash|
-      hash[:hoa] == "away" && hash[:won] == true
-    end
-    percent_home_wins = ((number_visitor_wins.count) / total_number_of_games).round(2)
+
+  def percentage_visitor_wins(data = nil)
+    data = @data if data == nil
+    data = subset_data(:hoa, "away", data)
+    (winning_percentage(data)).round(2)
   end
 
-  def count_of_games_by_season
-    seasons = Hash.new(0)
-    unique_seasons = find_unique_seasons
-    unique_seasons.each do |seasonid|
-      games = @data.select do |game_team_info|
-        game_team_info[:season] == seasonid
-      end
-      seasons[seasonid] = total_number_of_games(games)
-    end
-    return seasons
+  def count_of_games_by_season(data = nil)
+    data = @data if data = nil
+    group_id = :season
+    aggregate = :total_games
+    group_and_aggregate(group_id, aggregate, data)
   end
 
-  def average_goals_per_game(data_to_use = nil)
-    if data_to_use == nil
-      data_to_use = @data
+  def average_goals_per_game(data = nil)
+    if data == nil
+      data = @data
     end
-    total_goals = data_to_use.sum do |key, value|
-      key[:goals]
-    end
-    average_goals_per_game = (total_goals/total_number_of_games(data_to_use)).round(2)
+    hashID = :goals
+    total_goals = (find_total(hashID, data)).to_f
+    average = total_goals/total_games(data)
   end
 
-  def average_goals_by_season
-    unique_seasons = find_unique_seasons
-
-    average_goals_in_season = Hash.new
-
-    unique_seasons.each do |season_id|
-      games_in_season = @data.find_all do |game_team_info|
-        game_team_info[:season] ==  season_id
-      end
-    average_goals_in_season[season_id] = average_goals_per_game(games_in_season)
-    end
-    return average_goals_in_season
-  end
-
-  def total_number_of_games(data_to_use = nil)
-    if data_to_use == nil
-      data_to_use = @data
-    end
-    total_number_of_games = ((data_to_use.count)/2).to_f
-  end
-
-  def find_unique_seasons
-    @data.map do |game_team_info|
-      game_team_info[:season]
-    end.uniq
+  def average_goals_by_season(data = nil)
+    data = @data if data == nil
+    aggregate = :average_goals_per_game
+    subset_group_and_aggregate(nil, :season, aggregate)
   end
 end
