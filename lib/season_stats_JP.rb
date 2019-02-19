9ef9828a0106d7500791871a526807a5a2f26dd1
Jalena Penaligon <jalena.penaligon@gmail.com>
2019-02-19 13:39:26 -0700

    Updated methods to simplify

--- lib/season_stats_JP.rb
+++ lib/season_stats_JP.rb
@@ -12,84 +12,46 @@ module SeasonStatsJP
     total_games(subset_data(:won, true, data))
   end
 
-  def games_by_type(season, type, data = nil)
-    data = @data if data == nil
-    subset = {season: season.to_i, type: type}
-    group = :teamname
-    agg = :total_games
-
-    subset_group_and_aggregate(subset, group, agg)
-  end
-
-  def wins_by_type(season, type, data = nil)
-    data = @data if data == nil
-    subset = {season: season.to_i, type: type}
-    group = :teamname
-    agg = :total_wins
-
-    subset_group_and_aggregate(subset, group, agg)
-  end
-
-  def calculate_win_percentage(games, wins)
-    percentage = Hash.new(0)
-    wins.each do |team, wins|
-      percentage[team] = (wins /= games[team].to_f).round(2)
-    end
-    percentage
-  end
-
-  def win_percentage_by_type(season, type, data = nil)
-    games = games_by_type(season, type, data = nil)
-    wins = wins_by_type(season, type, data = nil)
-
-    calculate_win_percentage(games, wins)
-  end
-
-  def win_percentage_difference(season)
-    preseason = win_percentage_by_type(season, "P", data = nil)
-    regular = win_percentage_by_type(season, "R", data = nil)
-
-    difference = Hash.new(0)
-    preseason.each do |team, win_percent|
-      difference[team] = (win_percent -= regular[team]).round(2)
-    end
-    difference
-  end
-
   def biggest_bust(season)
-    find_max(win_percentage_difference(season))
+    subset = {season: season.to_i}
+    group = :teamname
+    agg = :win_percentage_difference
+
+    find_max(subset_group_and_aggregate(subset, group, agg))
+  end
+
+  def calculate_win_percentage(data = nil)
+    data = @data if data == nil
+    wins = total_wins(data)
+    games = total_games(data)
+
+    win_percentage = wins / games.to_f
+  end
+
+  def win_percentage_difference(data = nil)
+    data = @data if data == nil
+    preseason = calculate_win_percentage(subset_data(:type, "P", data))
+    regular = calculate_win_percentage(subset_data(:type, "R", data))
+
+    difference = preseason - regular
+
   end
 
   def biggest_surprise(season)
-    find_min(win_percentage_difference(season))
-  end
-
-  def games_by_coach(season)
-    data = @data if data == nil
     subset = {season: season.to_i}
-    group = :head_coach
-    agg = :total_games
+    group = :teamname
+    agg = :win_percentage_difference
 
-    subset_group_and_aggregate(subset, group, agg)
-  end
-
-  def wins_by_coach(season, won = true, data = nil)
-    data = @data if data == nil
-    subset = {season: season.to_i, won: won}
-    group = :head_coach
-    agg = :total_games
-
-    subset_group_and_aggregate(subset, group, agg)
-  end
-
-  def coach_winning_percentage(season)
-    games = games_by_coach(season)
-    wins = wins_by_coach(season)
-
-    calculate_win_percentage(games, wins)
+    find_min(subset_group_and_aggregate(subset, group, agg))
   end
 
   def winningest_coach(season)
-    find_max(coach_winning_percentage(season))
+    data = @data if data == nil
+    subset = {season: season.to_i}
+    group = :head_coach
+    agg = :calculate_win_percentage
+
+    find_max(subset_group_and_aggregate(subset, group, agg))
   end
+
 end
