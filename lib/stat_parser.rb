65f63ad6044004beb6c0e85a634c1c09d73cdaa0
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-20 16:35:50 -0700

    removed comments

--- lib/stat_parser.rb
+++ lib/stat_parser.rb
@@ -89,38 +89,3 @@ class StatParser
   end
 
 end
-
-
-### @array_raw_data is three arrays of hashes
-### 1st hash is of game_team data
-### 2nd hash is of  game data
-### 3rd hash is team_info data
-
-### array_merge_keys is two keys -- :game_id, :team_id
-
-### game_team and game are merged with :game_id
-### game/game_team and team_info are merged with :team_id
-
-### Desired keys are:
-# {game_id, team_id, hoa, won, settled_in, head_coach,
-#  goals, shots, hits, pim, powerplaygoals, powerplayopportunities,
-#  faceoffwinpercentage, giveaways, takeaways, teamname, season,
-#  type, date_time, venue_time_zone_tz}
-
-# game_team has a game_id
-# game (data) has a UNIQUE game_id
-# All data from game <- that has certain game_id
-###   Should be added to each hash of game_team with that game_id
-
-
-### gt = [{id: 1, t:1, s:3},
-###       {id:1, t:2, s:4}
-### g = [{id:1, v:6}]
-
-### h_a = [{id:1, t:1, s:3, v:6},
-###        {id:1, t:2, s:4, v:6}]
-
-### game_team.map do |gt_hash|
-###   to_merge = game.find{|g_hash| g_hash[:game_id] == gt_hash[:game_id]}
-###   gt_hash.merge(to_merge)
-### end
