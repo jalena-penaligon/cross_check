2929ca78902a21a6f99f6b625c0bb88cd0c5a3a7
Smitha Hosmani <smitha_hosmani@yahoo.com>
2019-02-20 16:35:17 -0700

    more refactoring

--- lib/season_stats.rb
+++ lib/season_stats.rb
@@ -3,14 +3,14 @@ module SeasonStats
   def biggest_bust(season)
     subset = {season: season.to_i}
     group = :teamname
-    agg = :win_percentage_difference
+    agg = :pre_reg_season_win_percentage_difference
     find_max(subset_group_and_aggregate(subset, group, agg))
   end
 
   def biggest_surprise(season)
     subset = {season: season.to_i}
     group = :teamname
-    agg = :win_percentage_difference
+    agg = :pre_reg_season_win_percentage_difference
     find_min(subset_group_and_aggregate(subset, group, agg))
   end
 
@@ -66,57 +66,4 @@ module SeasonStats
     find_min(subset_group_and_aggregate(subsets, group_id, aggregate))
   end
 
-######################
-#### HELPERS BELOW ###
-######################
-
-  def win_percentage_difference(data = nil)
-    data = @data if data == nil
-    preseason_data = subset_data(:type, "P", data)
-    return 0 if preseason_data.length == 0
-    preseason = winning_percentage(preseason_data)
-    regular = winning_percentage(subset_data(:type, "R", data))
-
-    difference = preseason - regular
-  end
-
-  def winning_percentage(data = nil)
-    data = @data if data == nil
-    total_wins(data)/total_games(data).to_f
-  end
-
-  def shooting_percentage(data = nil)
-    data = @data if data == nil
-    total_goals(data)/total_shots(data).to_f
-  end
-
-  def total_shots(data = nil)
-    data = @data if data == nil
-    find_total(:shots, data)
-  end
-
-  def total_goals(data = nil)
-    data = @data if data == nil
-    find_total(:goals, data)
-  end
-
-  def total_hits(data = nil)
-    data = @data if data == nil
-    find_total(:hits, data)
-  end
-
-  def total_wins(data = nil)
-    data = @data if data == nil
-    total_games(subset_data(:won, true, data))
-  end
-
-  def total_games(data = nil)
-    data = @data if data == nil
-    data.uniq{ |hash| hash[:game_id] }.length
-  end
-
-  def total_goals(data= nil)
-    data = @data if data == nil
-    find_total(:goals, data)
-  end
 end
